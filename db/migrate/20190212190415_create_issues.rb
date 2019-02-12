a75d663feba52115ee0042c6e292370c62ed6c5c
mostlyclicks <carlos@mostlyclicks.com>
2019-02-12 12:06:33 -0700

    scaffold issue

---
+++ db/migrate/20190212190415_create_issues.rb
@@ -0,0 +1,10 @@
+class CreateIssues < ActiveRecord::Migration[5.2]
+  def change
+    create_table :issues do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
